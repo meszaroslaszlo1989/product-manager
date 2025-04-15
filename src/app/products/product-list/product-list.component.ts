ada6ab73f55c8ee9bd9b858e7db9f690478f841a
meszaroslaszlo <laszlo.meszaros.connect@gmail.com>
2025-04-15 19:31:12 +0200

    [WB-4] Add mat table for proudct list component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,32 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NotificationService } from '../../core/services/notification.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-  products: Product[] = [];
+  displayedColumns: string[] = ['name', 'price', 'actions'];
+  dataSource = new MatTableDataSource<Product>();
 
-  constructor(private productService: ProductService) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private productService: ProductService, private notificationService: NotificationService) {
   }
 
   ngOnInit(): void {
     this.loadProducts();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   delete(id: string) {
     this.productService.delete(id);
+    this.notificationService.success('Sikeres törlés');
     this.loadProducts();
   }
 
   private async loadProducts() {
-    this.products = await this.productService.getAll();
+    this.dataSource.data = await this.productService.getAll();
   }
 
 }
