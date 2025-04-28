10dd6f2021446d9b44144c6c7d51b4454cda0c3e
meszaroslaszlo <laszlo.meszaros.connect@gmail.com>
2025-04-28 19:12:41 +0200

    [WB-9] Add layout components for application

---
+++ src/app/core/components/layout/layout.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+  isSidenavOpen = true;
+  currentYear = new Date().getFullYear();
+
+  switchMenuSize() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
+}
