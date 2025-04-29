a309b29609532608e19bdc509886b90418284444
meszaroslaszlo <laszlo.meszaros.connect@gmail.com>
2025-04-29 19:06:28 +0200

    [WB-11] Add authguard for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,23 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductFormComponent } from './products/product-form/product-form.component';
 import { LayoutComponent } from './core/components/layout/layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './core/components/login/login.component';
+import { RegistrationComponent } from './core/components/registration/registration.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'registration', component: RegistrationComponent },
     {
         path: '',
         component: LayoutComponent,
+        canActivate: [AuthGuard],
         children: [
             { path: 'products', component: ProductListComponent },
             { path: 'products/add', component: ProductFormComponent },
             { path: 'products/edit/:id', component: ProductFormComponent },
             { path: '', redirectTo: '/products', pathMatch: "full" }
         ]
-    }
+    },
+    { path: '', redirectTo: '/login', pathMatch: "full" }
 ];
